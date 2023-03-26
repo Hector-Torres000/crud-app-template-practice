b4c3a8b714a4006e4e7aa29225f0721154cab4be
Hector Torres <h.s.torres96@gmail.com>
2023-03-26 01:32:32 -0700

    crud practice 26 march 2023

---
+++ server.js
@@ -0,0 +1,28 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+const streamers = {
+  kiara: 'chatterbugxo',
+  lari: 'laribug',
+  britt: 'brittballs',
+  unknown: 'unknown',
+};
+
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/api/:name', (request, response) => {
+  const streamerName = request.params.name.toLowerCase();
+  if (streamers[streamerName]) {
+    response.json(streamers[streamerName]);
+  } else {
+    response.json(streamers.unknown);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
