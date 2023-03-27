9e1e12fe023bc0ffcaef59ccfb3e1614d103f901
Hector Torres <h.s.torres96@gmail.com>
2023-03-26 23:33:10 -0700

    changing port number

--- server.js
+++ server.js
@@ -23,6 +23,6 @@ app.get('/api/:name', (request, response) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
