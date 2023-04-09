e32f02a0dd72513ba2bf561b6eee898a327caab0
Hector Torres <h.s.torres96@gmail.com>
2023-04-08 20:27:11 -0700

    class #40 practice test run

--- server.js
+++ server.js
@@ -1,94 +1,48 @@
-const { json } = require('body-parser');
 const express = require('express');
 const app = express();
-
-app.use(express.json());
-let yellowBooks = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
-app.get('/', (request, response) => {
-  const hello = '<h1>Hello World</h1>';
-  response.send(hello);
-});
-
-app.get('/api/persons', (request, response) => {
-  response.json(yellowBooks);
-});
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const person = yellowBooks.find((person) => person.id == id);
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
-});
-
-app.get('/info', (request, response) => {
-  const numPersons = yellowBooks.length;
-  const time = new Date();
-  response.send(`
-    <p>Phonebook has info for ${numPersons} people</p>
-    <p>${time}</p>
-    `);
-});
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  yellowBooks = yellowBooks.filter((person) => person.id != id);
-  response.status(204).end();
-});
-
-const generateId = () => {
-  const maxId =
-    yellowBooks.length > 0
-      ? Math.max(...yellowBooks.map((person) => person.id))
-      : 0;
-  return maxId + 1;
-};
-
-app.post('/api/persons', (request, response) => {
-  const names = yellowBooks.map((person) => person.name);
-  const person = request.body;
-  if (!person.name) {
-    return response.status(400).json({ error: 'name is missing' });
-  } else if (!person.number) {
-    return response.status(400).json({ error: 'number is missing' });
-  } else if (names.includes(person.name)) {
-    return response.status(400).json({ error: 'name already exist' });
-  }
-
-  const newPerson = {
-    id: generateId(),
-    name: person.name,
-    number: person.number,
-  };
-  yellowBooks = yellowBooks.concat(newPerson);
-  response.json(newPerson);
-});
+const MongoClient = require('mongodb').MongoClient;
 
 const PORT = 3001;
-app.listen(PORT, () => {
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+const dbConnectionString =
+  'mongodb+srv://hstorres96:Ely6Z46d2W02BXWm@cluster0.ae7mzee.mongodb.net/?retryWrites=true&w=majority';
+
+let db, quotes;
+MongoClient.connect(dbConnectionString, { useUnifiedTopology: true }).then(
+  (client) => {
+    console.log('Connected to database');
+    db = client.db('favorite-quotes');
+    quotes = db.collection('quotes');
+  }
+);
+
+app.get('/', (request, response) => {
+  db.collection('quotes')
+    .find()
+    .toArray()
+    .then((data) => {
+      response.render('index.ejs', { info: data });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+app.post('/addQuote', (request, response) => {
+  console.log(request.body);
+  db.collection('quotes')
+    .insertOne({ author: request.body.author, quote: request.body.quote })
+    .then((result) => {
+      console.log('added quote');
+      response.redirect('/');
+    })
+    .catch((err) => console.error(err));
+});
+
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
